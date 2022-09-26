dfe6881504d8ae50affdd1787e4a92310f9ecd88
VicVeronique <107785778+VicVeronique@users.noreply.github.com>
2022-09-26 10:28:10 +0300

    added animated weather icon component

--- src/Weather.js
+++ src/Weather.js
@@ -17,7 +17,7 @@ export default function Weather(props) {
       wind: response.data.wind.speed,
       city: response.data.name,
       humidity: response.data.main.humidity,
-      iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      icon: response.data.weather[0].icon,
       date: new Date(response.data.dt * 1000),
     });
   }
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherInfo(props) {
   return (
@@ -13,8 +14,9 @@ export default function WeatherInfo(props) {
       </ul>
       <div className="row mt-3">
         <div className="col-6">
-          <img src={props.data.iconUrl} alt="mostly cloudy" />
-
+          <span className="WeatherIcon">
+            <WeatherIcon code={props.data.icon} />
+          </span>
           <span className="temperature">
             {Math.round(props.data.temperature)}
           </span>
