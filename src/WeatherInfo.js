906831968f14721f33090ba786fcdb7116c21a00
VicVeronique <107785778+VicVeronique@users.noreply.github.com>
2022-10-12 16:02:02 +0300

    CSS update

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -2,6 +2,7 @@ import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
 import WeatherTemperature from "./WeatherTemperature";
+import "./WeatherInfo.css";
 
 export default function WeatherInfo(props) {
   return (
@@ -22,7 +23,6 @@ export default function WeatherInfo(props) {
         </div>
         <div className="col-6">
           <ul>
-            <li>Precipitation 15%</li>
             <li>Humidity: {props.data.humidity} %</li>
             <li>Wind: {props.data.wind} km/h</li>
           </ul>
